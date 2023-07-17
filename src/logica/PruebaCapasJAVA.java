a62830f4e8422a08e726f01dfa8a3cb135f1f5f8
NicolasPozzer <pozzernico@gmail.com>
2023-07-17 19:06:58 -0300

    publish-in-git

---
+++ src/logica/PruebaCapasJAVA.java
@@ -0,0 +1,25 @@
+package logica;
+
+import gui.Pantalla;
+
+
+public class PruebaCapasJAVA {
+
+    
+    public static void main(String[] args) {
+        
+        //Creo la pantalla y importo
+        Pantalla panta = new Pantalla();
+        
+        //hacer visible la pantalla
+        panta.setVisible(true);
+        
+        //a mi pantalla ponela relativa a NULL osea nada lo cual queda
+        //en el centro!
+        panta.setLocationRelativeTo(null);
+        
+        
+        
+    }
+    
+}
